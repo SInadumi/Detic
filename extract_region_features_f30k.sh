11c2c0f16d57e8db0102596081db6279e0c7236b
SInadumi <inazumi19@icloud.com>
2024-11-20 16:55:10 +0000

    update default settings

--- extract_region_features_f30k.sh
+++ extract_region_features_f30k.sh
@@ -1,17 +1,19 @@
 #!/usr/bin/env bash
 
 set -euxo pipefail
-readonly ROOT_DIR="/mnt/home/shun-i/dev/cl_mmref/data/f30k_ent_jp"
+readonly ROOT_DIR="path/to/cl_mmref/data/f30k_ent_jp"
 
 for CONFIG_FILE in "Detic_LCOCOI21k_CLIP_R5021k_640b32_4x_ft4x_max-size" "Detic_LCOCOI21k_CLIP_SwinB_896b32_4x_ft4x_max-size"
 do
     poetry run python -u tools/extract_region_features.py \
         --root-dir ${ROOT_DIR} \
         --dataset-name f30k_ent_jp \
-        --output-file-name ${CONFIG_FILE}_thresh-30 \
+        --output-file-name ${CONFIG_FILE} \
         --config-file "configs/${CONFIG_FILE}.yaml" \
         --vocabulary lvis \
-        --confidence-threshold 0.3 \
+        --confidence-threshold 0 \
+        --nms-threshold 0.5 \
+        --detections-per-image 128 \
         --opts \
         MODEL.WEIGHTS "models/${CONFIG_FILE}.pth" \
         SEED 10
--- extract_region_features_jcre3.sh
+++ extract_region_features_jcre3.sh
@@ -1,17 +1,18 @@
 #!/usr/bin/env bash
 
 set -euxo pipefail
-readonly ROOT_DIR="/mnt/home/shun-i/dev/cl_mmref/data/jcre3"
+readonly ROOT_DIR="path/to/cl_mmref/data/jcre3"
 
 for CONFIG_FILE in "Detic_LCOCOI21k_CLIP_R5021k_640b32_4x_ft4x_max-size" "Detic_LCOCOI21k_CLIP_SwinB_896b32_4x_ft4x_max-size"
 do
     poetry run python -u tools/extract_region_features.py \
         --root-dir ${ROOT_DIR} \
         --dataset-name jcre3 \
-        --output-file-name ${CONFIG_FILE}_thresh-30 \
+        --output-file-name ${CONFIG_FILE} \
         --config-file "configs/${CONFIG_FILE}.yaml" \
         --vocabulary lvis \
-        --confidence-threshold 0.3 \
+        --confidence-threshold 0 \
+        --nms-threshold 0.5 \
         --opts \
         MODEL.WEIGHTS "models/${CONFIG_FILE}.pth" \
         SEED 10
